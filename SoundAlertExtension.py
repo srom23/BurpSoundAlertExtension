78b946ca624d070f83ca99adefbc04d3fe32ff4b
srom23 <sunilkumarceoe1@gmail.com>
2024-02-11 00:21:45 +0530

    Update SoundAlertExtension.py
    Added threading to alert in diffrent thread so the code continue Run.

--- SoundAlertExtension.py
+++ SoundAlertExtension.py
@@ -1,15 +1,15 @@
-from burp import IBurpExtender, IProxyListener, IHttpListener
+from burp import IBurpExtender, IHttpListener
 from javax.swing import JOptionPane
 from java.awt import Toolkit
-
+from threading import Thread
 
 class BurpExtender(IBurpExtender, IHttpListener):
-    
+
     def registerExtenderCallbacks(self, callbacks):
         self.callbacks = callbacks
         self.helpers = callbacks.getHelpers()
         self.callbacks.setExtensionName("SoundAlertExtension")
-        self.sound_alert_string = 'Your_String'
+        self.sound_alert_string = '"availableOperations":[{"id":'
 
         # Register the listener
         callbacks.registerHttpListener(self)
@@ -22,7 +22,9 @@ class BurpExtender(IBurpExtender, IHttpListener):
 
             # Check if the specified string is in the response
             if self.sound_alert_string in self.helpers.bytesToString(response):
-                self.playSoundAlert()
+                # Run the alert in a separate thread
+                alert_thread = Thread(target=self.playSoundAlert)
+                alert_thread.start()
 
     def playSoundAlert(self):
         # Play a sound alert
